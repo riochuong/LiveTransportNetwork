f6254ed28d5f9f44c2741a405d9faec6f9a943c5
chuong.dao <riochuong@gmail.com>
2023-04-19 00:51:25 -0700

    project is finally build

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,12 @@ class ConanPackage(ConanFile):
     version = "0.1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
+    default_options = {"boost/*:shared": False}
     
-
+    def layout(self):
+        self.folders.build = f"build/{str(self.settings.build_type)}"
+        self.folders.generators = "build"
+    
+    def requirements(self):
+        self.requires("boost/1.81.0")
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
 #include <iostream>
 
-
-
 int main (void) {
-   std::cout << "It works" << std::endl;
-   return 0;
+   boost::system::error_code ec {};
+   if (ec) {
+      std::cerr << "Error: " << ec.message() << std::endl;
+      return -1;
+   } else {
+      std::cout << "It works" << std::endl;
+      return 0;   
+   }
 }
