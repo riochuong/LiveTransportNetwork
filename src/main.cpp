9cd4a577d2e2e47697c42493cc68f36d6a491481
chuong.dao <riochuong@gmail.com>
2023-04-20 23:04:14 -0700

    able to communicate with echo server

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
-#include <boost/asio.hpp>
-#include <boost/system/error_code.hpp>
 #include <iostream>
 #include <thread>
 #include <iomanip>
 #include <vector>
+#include <string>
+#include <cassert>
+
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/beast.hpp>
+//#include <boost/beast/ssl.hpp>
+
 
 using tcp = boost::asio::ip::tcp;
 using ip_address = boost::asio::ip::address;
 using boost_error_code = boost::system::error_code;
+namespace net = boost::asio;
+using namespace boost::beast;
+using namespace boost::beast::websocket;
 
 void Log(boost_error_code ec) {
    std::cerr << (ec ? "Error : " : "OK")
@@ -24,35 +33,36 @@ int main (void) {
    boost::asio::io_context ioc{};
 
    // make strand to serialize execution of handlers 
-   tcp::socket socket {boost::asio::make_strand(ioc)};
+   tcp::socket socket {ioc};
    tcp::resolver resolver {ioc};
-   boost::asio::ip::basic_resolver_results results {resolver.resolve("google.com", "80", ec)};
-   
-   size_t nThreads = 10;
-
-   std::vector<std::thread> tPools;
-   tPools.reserve(nThreads);
-
-   for (int i = 0; i < nThreads; i++){
-      // create bunch of async jobs
-      socket.async_connect(*results.begin(), OnConnect);
+   boost::asio::ip::basic_resolver_results results {resolver.resolve("ltnm.learncppthroughprojects.com", "80", ec)};
+   socket.connect(*results.begin());
+   stream<tcp_stream> websocket(std::move(socket));
+   // communicate via text format 
+   websocket.text(true);
+   websocket.handshake("ltnm.learncppthroughprojects.com", "/echo", ec);
+   Log(ec);
+   if (ec){
+      return -1;
    }
-   for (size_t i = 0; i < nThreads; i++){
-      tPools.emplace_back(
-         [&ioc](void){
-         // std::cout << "["
-         //           << std::setw(14) << std::this_thread::get_id()
-         //           << "] ioc.run()"
-         //           << std::endl;
-         ioc.run();
+   ec.clear();
+   std::string msg_orig = "Test 123";
+   size_t sent_bytes = websocket.write(net::buffer(msg_orig));
+   std::cout << "Sent bytes: " << sent_bytes <<"\n";
+   std::string echo_msg;
+   boost::asio::dynamic_string_buffer buffer{echo_msg};
+   try{
+      int num_bytes = websocket.read(buffer, ec);
+      if (ec) {
+         std::cerr << "Error reading data: " << ec.message() <<"\n";
+         return -3;
       }
-      );
-   }
-   // call ioc.run() on a thread 
-   for (auto& t: tPools){
-      t.join();
-   }
-   
-   return 0;
-
+      std::cout << "Read bytes: " << num_bytes << "\n";
+      assert(num_bytes == msg_orig.size());
+      std::cout << "Echo message: " << echo_msg << std::endl;
+      return 0;
+   } catch (boost::system::system_error e){
+      std::cerr << "Error: " << e.what() << "\n";
+   };
+   return -1;
 }
